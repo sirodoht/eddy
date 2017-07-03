44698ea42cf5ff84afdc6eb13030224b93776562
Daniel Lockyer <thisisdaniellockyer@gmail.com>
2017-07-03 18:32:51 +0100

    Fix clippy lints

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,21 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let mut argument_spaces = "2".to_string();
-    if args.len() == 2 {
-        argument_spaces = args[1].clone().to_string();
-    }
+    let mut argument_spaces = if args.len() == 2 {
+        args[1].clone().to_string()
+    } else {
+        "2".to_string()
+    };
 
-    let mut argument_path = (".").to_string();
-    if args.len() == 3 {
+    let argument_path = if args.len() == 3 {
         argument_spaces = args[1].clone().to_string();
-        argument_path = args[2].clone().to_string();
-    }
+        args[2].clone().to_string()
+    } else {
+        ".".to_string()
+    };
 
     let file_name = "/.editorconfig";
-    let full_path = argument_path + &file_name;
+    let full_path = argument_path + file_name;
 
     let save_path = Path::new(&full_path);
     let save_path_display = save_path.display();
@@ -43,10 +45,9 @@ trim_trailing_whitespace = false\n");
 
     let contents = part_1 + &argument_spaces + &part_2;
 
-    let mut output_file = match File::create(&save_path) {
-        Err(error) => panic!("Could not create {}: {}", save_path_display, error.description()),
-        Ok(file) => file,
-    };
+    let mut output_file = File::create(&save_path).unwrap_or_else(|error| {
+        panic!("Could not create {}: {}", save_path_display, error.description());
+    });
 
     output_file.write_all(contents.as_bytes()).unwrap();
 }
